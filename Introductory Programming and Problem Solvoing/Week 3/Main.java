b0232a0085dbb3eb0aa7277af858e0e2c97a39a1
Manoj Neupane <manpane98@gmail.com>
2022-12-18 10:55:57 +0545

    week 4 and 5

--- Introductory Programming and Problem Solvoing/Week 3/Main.java
+++ Introductory Programming and Problem Solvoing/Week 3/Main.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 import java.util.Scanner;
 public class Main {
     static HashMap<Integer,ArrayList<Integer>> memo = new HashMap<>();
-    static final int[] currencies = {1000,500,100,50,20,10,5,2,1};
+    static final int[] currencies = {50,20,10,5,2,1};
     public static void main(String[] args) {
         System.out.print("Enter the change: ");
         int change = new Scanner(System.in).nextInt();
-        ArrayList<Integer> combination = findSum(change, currencies);
+        ArrayList<Integer> combination = findCoinCombination(change, currencies);
         if(combination!=null){
             HashMap<Integer,Integer> map = new HashMap<>();
             for(int a:combination){
@@ -26,53 +26,24 @@ public class Main {
             System.out.println("\nTotal\t\t\t\t"+change);
         }
     }
-
-    /**
-     * 
-     * @param sum the sum to make with given list of coins
-     * @param coins the coins to make the sum with
-     * @return null or ArrayList<Integer> with the best combination of coins to make to the sum
-     * 
-     * @baseCases
-     * @
-     * 1. IF the sum is already present in the memo object
-     * @
-     * 2. IF the sum is 0, the combination is found
-     * @
-     * 3. IF the sum is less than 0, combination not found
-     */
-    public static ArrayList<Integer> findSum(int sum,int[] coins){
-
-        //base cases
-        //if the change is already calculated, just return that
-        if(memo.get(sum) != null)return memo.get(sum);
-
-        // if the sum is 0, it means the change is found, returning empty array
-        //This empty array is filled as the recursive tree moves up
+    public static ArrayList<Integer> findCoinCombination(int sum,int[] coins){
+        if(memo.get(sum) != null)return memo.get(sum);//memo is a static class variable
         if(sum==0)return new ArrayList<Integer>(0);
-
-        //if sum is less than 0, no change is possible with current combination
         if (sum<0)return null;
-
-        //initialize the best combination as empty array
         ArrayList<Integer> bestSum  = new ArrayList<Integer>(0);
         
-        //main algorithm
         for(int paisa : coins){
-            int diff = sum-paisa;//calculate the remaining sum to calculate
-            ArrayList<Integer> comb = findSum(diff, coins);// finding the sum for remaining
-            
-            if (comb!=null){// if combination is available with the remaining
-                comb.add(paisa);// appending the current paisa to the list as it is valid combination
-                if(bestSum.size()!=0){//cheking the shortest combination
+            int diff = sum-paisa;
+            ArrayList<Integer> comb = findCoinCombination(diff, coins);
+            if (comb!=null){
+                comb.add(paisa);
+                if(bestSum.size()!=0){
                     if (comb.size()<bestSum.size()) bestSum = (ArrayList<Integer>) comb.clone();
-                }else bestSum = (ArrayList<Integer>) comb.clone();//if no combination is choosen, setting the bestsum as current combination
+                }else bestSum = (ArrayList<Integer>) comb.clone();
             }
         }
-        if (bestSum.size()==0){//bestsum size 0 means no combination found
-            return null;
-        }
-        memo.put(sum,bestSum);//putting the calculated best combination for future use
+        if (bestSum.size()==0)return null;
+        memo.put(sum,bestSum);
         return bestSum;
     }
 
@@ -87,6 +58,5 @@ public class Main {
         }
         return value;
     }
-
     public static long factorial(int n){return n<1?1:factorial(n-1)*n;}
 }
---
+++ Introductory Programming and Problem Solvoing/Week 3/Test.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Test{
+    public static void main(String[] args) {
+        
+    }
+    public static void him(){
+
+    }
+}
